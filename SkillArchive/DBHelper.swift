9652d0d44b9fef0c64af3e9e3996c5f5cbd1c538
SpazzStuff <36232417+SpazzStuff@users.noreply.github.com>
2022-08-01 16:43:37 -0400

    Inserting Data tutorial step

--- SkillArchive/DBHelper.swift
+++ SkillArchive/DBHelper.swift
@@ -17,6 +17,7 @@ class DBHelper {
     init() {
         db = openDatabase()
         createTable()
+        insert()
     }
     
     let dbPath: String = "archiveDb.sqlite"
@@ -36,7 +37,7 @@ class DBHelper {
     
     func createTable() {
         // optional values could be a problem
-        let createTableString = "CREATE TABLE IF NOT EXISTS Skill(Id INTEGER PRIMARY KEY,Name TEXT,Image TEXT,Video TEXT,Note TEXT"
+        let createTableString = "CREATE TABLE IF NOT EXISTS Skill(Id INTEGER PRIMARY KEY,Title TEXT,Image TEXT,Video TEXT,Note TEXT"
         var createTableStatement: OpaquePointer? = nil
         if sqlite3_prepare_v2(db, createTableString, -1, &createTableStatement, nil) == SQLITE_OK {
             if sqlite3_step(createTableStatement) == SQLITE_DONE {
@@ -48,6 +49,27 @@ class DBHelper {
             print("CREATE TABLE statement is not prepared")
         }
         sqlite3_finalize(createTableStatement)
+    }
+    
+    let insertStatementString = "INSERT INTO Skill (Id, Title, Image, Video, Note) VALUES (?, ?, ?, ?, ?);"
+    
+    func insert() {
+        var insertStatement: OpaquePointer?
+        if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
+            let id: Int32 = 1  //temp
+            let title: NSString = "test"  //temp
+            sqlite3_bind_int(insertStatement, 1, id)
+            sqlite3_bind_text(insertStatement, 2, title.utf8String, -1, nil)
+            //add more
+            if sqlite3_step(insertStatement) == SQLITE_DONE {
+                print("Successfully inserted row")
+            } else {
+                print("Could not insert row")
+            }
+        } else {
+            print("INSERT statement is not prepared")
+        }
+        sqlite3_finalize(insertStatement)
     }
     
 }
